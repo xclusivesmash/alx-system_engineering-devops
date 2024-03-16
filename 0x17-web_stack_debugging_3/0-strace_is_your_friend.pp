9745a83ba79a7895f143598f6d8ae80ebd0557ae
xclusivesmash <sbumatshiane916@gmail.com>
2024-03-17 00:53:01 +0200

    Initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# debugging script
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
