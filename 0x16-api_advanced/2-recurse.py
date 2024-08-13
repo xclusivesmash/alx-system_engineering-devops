6e0e8508580d9ba4eaba468f1f4fb56077c7b731
xclusivesmash <sbumatshiane916@gmail.com>
2024-08-13 14:45:10 -0700

    Updated code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Description: Query Reddit API recursively for all hot articles of a given subreddit
+Description: Query Reddit API recursively for all hot articles
 """
 import requests
 
@@ -10,18 +10,25 @@ def recurse(subreddit, hot_list=[], after="tmp"):
         return all hot articles for a given subreddit
         return None if invalid subreddit given
     """
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if after != "tmp":
-        url = url + "?after={}".format(after)
-    rq = requests.get(url, headers=headers, allow_redirects=False)
-    results = rq.json().get('data', {}).get('children', [])
-    if not results:
-        return hot_list
-    for e in results:
-        hot_list.append(e.get('data').get('title'))
-    after = rq.json().get('data').get('after')
-    if not after:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    parameters = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=parameters,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    results = response.json().get("data")
+    after = results.get("after")
+    count = count + results.get("dist")
+    for m in results.get("children"):
+        hot_list.append(m.get("data").get("title"))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
