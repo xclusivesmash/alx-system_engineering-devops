01f3e83d85f9b53120c9d0532866334b2c2b7afb
xclusivesmash <sbumatshiane916@gmail.com>
2024-08-14 12:11:20 -0700

    Updated code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,12 @@ def number_of_subscribers(subreddit):
     """return number of subscribers for a given subreddit
         return 0 if invalid subreddit given.
     """
+    headers = {
+        "User-Agent": "API/siphamandla"
+        }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': "Mozilla/5.0"}
-    rs = requests.get(url, headers=headers, allow_redirects=False)
-    if rs.status_code == 200:
-        data = rs.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    sub = response.json().get("data").get("subscribers")
+    return sub
